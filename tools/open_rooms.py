7e6afc56ed404d539f13e18b72610b3341932f43
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2024-08-29 11:50:09 +0200

    Open the rooms to access the server

---
+++ tools/open_rooms.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import os
+import json
+import sys
+
+CRANIX_FW_CONFIG="/etc/cranix-firewall.conf"
+config = json.load(open(CRANIX_FW_CONFIG))
+device = config['devices']['internal']
+
+for room in json.load(os.popen('crx_api.sh GET rooms/all')):
+    try:
+        ip_range=f"{room['startIP']}/{room['netMask']}"
+        command = f"/usr/sbin/iptables -A INPUT -i {device} -s {ip_range} -j ACCEPT"
+        os.system(command)
+    except:
+        print("open_rooms error", room)
+
