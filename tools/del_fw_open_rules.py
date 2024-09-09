54cf0381e297a83ff5b17a7dcdfd83a0f8c58820
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2024-09-09 09:34:38 +0200

    New tools to add and remove open_rules

---
+++ tools/del_fw_open_rules.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Copyright 2024 Dipl. Ing. Peter Varkoly <pvarkoly@cephalix.eu>. All rights reserved.
+import json
+import os
+CRANIX_FW_CONFIG="/etc/cranix-firewall.conf"
+
+rule=input("")
+config = json.load(open(CRANIX_FW_CONFIG))
+if rule in config["open_rules"]:
+    config["open_rules"].remove(rule)
+    with open(CRANIX_FW_CONFIG,"w") as f:
+        json.dump(config, f, indent=True)
+    os.system("/usr/sbin/crx_firewall.py")
