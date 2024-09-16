88687ebe373779f6017ca88d61346c250085c9af
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2024-09-16 18:12:01 +0200

    Use the full path.

--- tools/open_rooms.py
+++ tools/open_rooms.py
@@ -7,7 +7,7 @@ CRANIX_FW_CONFIG="/etc/cranix-firewall.conf"
 config = json.load(open(CRANIX_FW_CONFIG))
 device = config['devices']['internal']
 
-for room in json.load(os.popen('crx_api.sh GET rooms/all')):
+for room in json.load(os.popen('/usr/sbin/crx_api.sh GET rooms/all')):
     try:
         ip_range=f"{room['startIP']}/{room['netMask']}"
         command = f"/usr/sbin/iptables -A INPUT -i {device} -s {ip_range} -j ACCEPT"
